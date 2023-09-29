5a07be440d43012f335e3fe4455e894aebb6788c
Thandeka93 <noluthandombali4@gmail.com>
2023-09-29 09:21:45 +0200

    filter f

--- regNumber.js
+++ regNumber.js
@@ -18,29 +18,5 @@ export default function LicensePlateValidator() {
   };
 }
 
-// export default function LicensePlateValidator() {
-//   const licensePlateRegex = /^(CA|CL|CJ|CY)\s\d{3}(-? ?\d{1,3})$/i;
-//   let errorMessage;
-//   const registeredLicensePlates = new Set();
 
-//   function validateLicensePlate(licensePlate) {
-//     errorMessage = "";
-    
-//     if (!licensePlateRegex.test(licensePlate)) {
-//       errorMessage = "Please enter a valid registration number";
-//     } else if (licensePlate === "") {
-//       errorMessage = "Please provide a registration number";
-//     } else if (registeredLicensePlates.has(licensePlate)) {
-//       errorMessage = "Registration number already exists";
-//     } else {
-//       registeredLicensePlates.add(licensePlate);
-//     }
-    
-//     return errorMessage;
-//   }
-
-//   return {
-//     validateLicensePlate,
-//   };
-// }
 
